720622d15f9336da814b9021a8df65af1eb1b120
Josh Schmidt <oyejorge@gmail.com>
2012-11-11 23:54:04 -0700

    navbar positioning

--- js/ui/navbar.js
+++ js/ui/navbar.js
@@ -13,29 +13,21 @@ $.fn.findernavbar = function(fm, opts) {
 				}),
 			wz     = parent.children('.finder-workzone').append(nav),
 			delta  = nav.outerHeight() - nav.height(),
-			ltr    = fm.direction == 'ltr',
 			handle;
 
 
 		if ($.fn.resizable) {
 			handle = nav.resizable({
-					handles : ltr ? 'e' : 'w',
+					handles : (fm.direction == 'ltr') ? 'e' : 'w',
 					minWidth : opts.minWidth || 150,
 					maxWidth : opts.maxWidth || 500
 				})
 				.bind('resize scroll', function() {
 					handle.css({
 						top  : parseInt(nav.scrollTop())+'px',
-						left : parseInt(ltr ? nav.width() + nav.scrollLeft() - handle.width() - 2 : nav.scrollLeft() + 2)
 					})
 				})
 				.find('.ui-resizable-handle').zIndex(nav.zIndex() + 10);
-
-			if (!ltr) {
-				nav.resize(function() {
-					nav.css('left', null).css('right', 0);
-				});
-			}
 
 			fm.one('open', function() {
 				setTimeout(function() {

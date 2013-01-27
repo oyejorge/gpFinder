851cb089d1a8f075605cfe83ddd598ab38f40650
Josh Schmidt <oyejorge@gmail.com>
2013-01-27 10:26:00 -0700

    pt_BR 'Permissões'

--- js/i18n/pt_BR.js
+++ js/i18n/pt_BR.js
@@ -167,7 +167,7 @@ if (Finder && Finder.prototype && typeof(Finder.prototype.i18) == 'object') {
 			'apllyAll'        : 'Aplicar a todos',
 			'name'            : 'Nome',
 			'size'            : 'Tamanho',
-			'perms'           : 'Permições',
+			'perms'           : 'Permissões',
 			'modify'          : 'Modificado',
 			'kind'            : 'Tipo',
 			'read'            : 'Ler',

0bd65789770c0cea6d64e5fd3d2bcdcf22a40ae3
Josh Schmidt <oyejorge@gmail.com>
2013-01-27 10:40:46 -0700

    fix rename

--- php/FinderVolumeDriver.class.php
+++ php/FinderVolumeDriver.class.php
@@ -1259,14 +1259,11 @@ abstract class FinderVolumeDriver {
 
 		$this->rmTmb($file); // remove old name tmbs, we cannot do this after dir move
 
-		if (!$this->_move($path, $dir, $name)) {
-			return false;
+		if (($path = $this->_move($path, $dir, $name))) {
+			$this->clearcache();
+			return $this->stat($path);
 		}
-
-		$path = $this->_joinPath($dir, $name);
-
-		$this->clearcache();
-		return $this->stat($path);
+		return false;
 	}
 
 	/**
